c09267c39dfe699ca0956cd94c3f26425b4ed214
Prakash Rao <mb16@iitbbs.ac.in>
2022-10-28 21:29:59 +0900

    added sg, key-pair and ec2 pubilc o/p

--- main.tf
+++ main.tf
@@ -7,10 +7,45 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-northeast-1"
+  region = "us-east-1"
+}
+
+
+
+resource "aws_security_group" "dynamic_sg" {
+  name = "dynamic_sg"
+  description = "ingress and egress for ec2"
+  dynamic "ingress" {
+    for_each = var.sg_ports
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "test_keypair" {
+    key_name = "test_keypair"
+    public_key = file("/Users/prakashraobethapudi/Desktop/github/key_pair.pem.pub")
 }
 
 resource "aws_instance" "myec2" {
   ami = var.ami
   instance_type = var.instance_type
+  security_groups = [aws_security_group.dynamic_sg.name]
+  key_name = aws_key_pair.test_keypair.key_name
 }
+
+output "ec2_global_ips" {
+  value = "${aws_instance.myec2.*.public_ip}"
+}
+
--- variables.tf
+++ variables.tf
@@ -5,3 +5,11 @@ variable "instance_type" {
 variable "ami" {
   default = "ami-09d3b3274b6c5d4aa"
 }
+
+variable "sg_ports" {
+  type = list(number)
+  description = "sg ingress ports"
+  default = [ 80,22 ]
+}
+
+
